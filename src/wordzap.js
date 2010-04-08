eb066880c0cd7f6c941163f3dda47104daaf336e
Kowshik Prakasam <kowshik@gmail.com>
2010-04-08 18:14:42 +0530

    Minor changes

--- src/wordzap.js
+++ src/wordzap.js
@@ -1,5 +1,26 @@
-function setHeight()
+function setHeight(pageName)
 {
-	var content=document.getElementById('content');
-	content.style.height=""+screen.height+"px";
+	
+
+	var minHeights = new Array();
+	
+	minHeights['home']=690;
+	minHeights['version_history']=690;
+	minHeights['installation']=3200;
+	minHeights['screenshots']=690;
+	minHeights['source_code']=690;
+	minHeights['authors_contact']=690;
+	minHeights['contribute']=690;
+	minHeights['license']=690;
+	
+	if(screen.height < minHeights[pageName])
+	{
+		alert("New size : "+minHeights[pageName]);
+		document.getElementById('content').style.height=''+minHeights[pageName]+'px';
+	}
+	else
+	{
+			document.getElementById('content').style.height=screen.height+'px';
+	}
+	
 }
